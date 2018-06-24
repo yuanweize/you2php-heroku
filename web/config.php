d18a28f211a24c7a0d318d5586338dc2f18fdbfe
iYuanWeize <1835352233@qq.com>
2018-06-24 17:29:37 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -1,7 +1,7 @@
 <?php
 define('ROOT_PART', Root_part());
 define('APIKEY', 'AIzaSyDIOPOkZ9GHecSueM6ZOuryOr3rJnfH0ko');//这里改成您的api key
-define('GJ_CODE', 'HK');//国家代码
+define('GJ_CODE', 'US');//国家代码
 define('SITE_NAME', 'Youtube');//网站名字
 define('TITLENAME', 'Youtube');//网站名字
 define('EN2DEKEY', 'AnQUP1XDEF435e67CtzDSh4');//一个随机字符串，加解密KEY
